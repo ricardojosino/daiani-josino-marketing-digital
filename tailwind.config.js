26fa629630e68f758eecf57ac9051edff713aa49
ricardojosino <ricardojosino@gmail.com>
2022-09-23 20:21:24 -0300

    Criado página de política de privacidade e termo de uso

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,32 @@ module.exports = {
 
   content: [
       "./src/*.{html,js}",
-      "./src/index.html"
+      "./src/index.html",
+      "./src/politica-de-privacidade/index.html",
+      "./src/termos-de-uso/index.html",
   ],
 
   theme: {
 
-    colors : {
-      'color01' : '#1A1A1A',
-      'color02' : '#034D5A',
-      'color03' : '#CBA340',
-      'color04' : '#EDE6A4',
-      'color05' : '#D8DBEE',
-      'color06' : '#F4F4F4',
-      'color07' : '#FFFFFF'
-    },
-
-    container : {
-      center : true
-    },
-
-    fontFamily : {
-      'montserrat' : 'Montserrat'
-    },
-
     extend: {
+
+      colors : {
+        'color01' : '#1A1A1A',
+        'color02' : '#034D5A',
+        'color03' : '#CBA340',
+        'color04' : '#EDE6A4',
+        'color05' : '#D8DBEE',
+        'color06' : '#F4F4F4',
+        'color07' : '#FFFFFF'
+      },
+
+      container : {
+        center : true
+      },
+
+      fontFamily : {
+        'montserrat' : 'Montserrat'
+      },
 
       screens : {
         'xl' : '1140px'
